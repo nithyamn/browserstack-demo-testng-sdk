210743ac4343a43c36b6e23f6b7165cda19617a6
Nithya Mani <nithya.mn999@gmail.com>
2023-05-15 16:32:41 +0530

    few changes

--- src/test/java/web/BaseTestWeb.java
+++ src/test/java/web/BaseTestWeb.java
@@ -1,4 +1,5 @@
 package web;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
@@ -14,9 +15,11 @@ public class BaseTestWeb {
     @BeforeMethod(alwaysRun = true)
     @SuppressWarnings("unchecked")
     public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("start-maximized");
+        options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
